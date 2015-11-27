86d5606a5972c012e3a0c63a6a5d9f5dcc8f2d5d
akarabasov <a.v.karabasov@gmail.com>
2015-11-27 11:22:15 +0500

    test

---
+++ src/main/java/me/student/lemur/Application.java
@@ -0,0 +1,11 @@
+package me.student.lemur;
+
+/**
+ * Created by user on 17/11/15.
+ */
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Lemurs are cool!");
+        System.out.println("Lemurs are cool! 2");
+    }
+}
